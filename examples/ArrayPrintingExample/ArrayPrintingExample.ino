a14b10f194673439a7dbfd40ddc5b394f85b3e54
Jacob Smith <jsmith2021@brandeis.edu>
2019-06-25 01:17:23 -0400

    saved example sketch with comments

--- examples/ArrayPrintingExample/ArrayPrintingExample.ino
+++ examples/ArrayPrintingExample/ArrayPrintingExample.ino
@@ -1,15 +1,26 @@
 /*Written by Jacob Smith for Arduino Outreach Project
 Shows how arrays can be used in Arduino Classes to allow the 
 user to input an array without having to specify its size
-June 24 2019*/
-
+June 24 2019
+Sources:
+https://forum.arduino.cc/index.php?topic=508126.0
+https://blog.feabhas.com/2013/11/shock-horror-i-learned-something-about-arrays-in-c/
+*/
+//set up the ArduinoEducation background code
+#include <ArduinoEducation.h>
 ArduinoEducation test;
+//the setup method runs once
 void setup() {
-  int pins={5,6,7};
-  test.begin(pins);
+  //connect the arduino to the computer
+  Serial.begin(9600);
+  //create a list of pin numbers
+  int pins []={5,6,7,8,9,11,12};
+  //pass the pins to the test program, and also give the list size
+  test.begin(pins,ARRAY_SIZE(pins));
 
 }
-
+//the loop method runs many times
 void loop() {
+  //print the pins to the screen to show they where saved
   test.printPins();
 }
