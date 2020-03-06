5e6da53532fab6ebc77b118e52571c8b9d1a4486
Jacob Smith <jsmith2021@brandeis.edu>
2020-03-06 01:29:41 -0500

    added speaker tactor example and fixed onoff threshold

--- examples/tactorTimeSpeaker/tactorTimeSpeaker.ino
+++ examples/tactorTimeSpeaker/tactorTimeSpeaker.ino
@@ -1,4 +1,4 @@
-/* Prints distance to screen
+/* vibrates tactor for x milliseconds base don how far away object is
    Created from example program and turned into class by Jacob Smith
 */
 //includes the libraries of code necessary to make this one work
@@ -7,15 +7,17 @@
 //The object used to interfact with the class
 DistanceSensor distancesensor;
 //distance sensor sees
-double distance;
-//the port of the vibrator
+int distance;
 int vibPin=2;
+int buzzerPin=3;
+//the port of the vibrator
 //runs once
 void setup() {
   //sets up the class
   distancesensor.begin(10, 11);
   //sets up the class with minimum and maximum values
   Serial.begin(9600);
+  //set vibrator to output
   pinMode(vibPin, OUTPUT);
 
 }
@@ -26,12 +28,15 @@ void loop() {
   distance = distancesensor.getDistance();
   //display distance to screen
   Serial.println(distance);
-  //vibrates if distance is within range
+  //if distance is withing range, pulse depending on range
   if (distance < 80 && distance>5) {
+    // turn the port on
+    tone(buzzerPin,distance*-5+700);
     digitalWrite(vibPin, HIGH);   
-  //otherwise turns fibrator off and waits x milliseconds
-  } else {                  
-    digitalWrite(vibPin, LOW);    
-  }
-  delay(500);
+    //wait for x milliseconds
+    delay(distance*10);
+    digitalWrite(vibPin, LOW);
+    noTone(buzzerPin);
+    delay(distance*10); 
+  }  
 }
