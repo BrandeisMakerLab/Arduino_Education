be7654086abeceab0ee5b17908a306424550f81e
Jacob Smith <jsmith2021@brandeis.edu>
2020-04-10 16:02:50 -0400

    added new speaker example file so it can work with other sensors that use a timer

--- examples/Speaker/Speaker.ino
+++ examples/Speaker/Speaker.ino
@@ -2,14 +2,20 @@
  * controls a passive buzzer plugged in to ports 11 and gnd in Arduino
  * Curated by Jacob Smith Brandeis University 
 */
-int buzzerPin=11;
+//the digital port the buzzer is in
+int buzzerPin=10;
+//how long each note will be
 int time=500;
+//runs once to set up program
 void setup() {
-  // put your setup code here, to run once:
-
+  //specifies that the speaker port will be an output
+   pinMode(buzzerPin, OUTPUT);
 }
+//runs many times
 void loop() {
+  //plays a tone on the buzzer, the number is the note to play
   tone(buzzerPin,300);
+  //
   delay(time*2);
   tone(buzzerPin,400);
   delay(time*2);
