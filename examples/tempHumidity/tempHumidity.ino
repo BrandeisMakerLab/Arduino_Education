69428e209fb3732c1ab3276b8c5f5f668b7bb75a
Jacob Smith <jsmith2021@brandeis.edu>
2020-11-07 22:15:28 -0500

    updated temphumdity exmaple to be simpler

--- examples/tempHumidity/tempHumidity.ino
+++ examples/tempHumidity/tempHumidity.ino
@@ -1,40 +1,37 @@
+/* Written by Jacob Smith for Brandeis Automation Laboratory 11/7/20
+ *  Demonstrates the use of teh DHT Humidt and temperature sensor
+ *  Note: requires DHT background library, which can be installed automatically
+ * Wiring: S-->Digital 3, Power-->3.3V, - --> GND
+*/
+
+//dht type, resistor, timing
 //imports the library that must be used to run this program
 #include <DHT.h>
-#define BLUEPIN 4
-#define REDPIN 3
-//initializes the part so that the program knows what to expect to run
-DHT dht(2,DHT22);
-float initHumid;
+
+//sets up the sensor, plugged into digital port 2
+DHT dht(3,DHT11);
+
 void setup() {
   //initialize serial port (allows for commuication with arduino by clicking on the magnifying glass
   //on the top right corner of the window AFTER the program is uploaded
   Serial.begin(9600);
-   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(BLUEPIN, OUTPUT);
-  pinMode(REDPIN, OUTPUT);
   //starts the loop
   dht.begin();
-  initHumid = dht.readHumidity();
+  Serial.println("Humidity\tTemperature");
 }
 void loop() {
-  float humidity = dht.readHumidity();
-  Serial.print("init");
-  Serial.println(initHumid);
-  Serial.print("Humidity: ");
-  Serial.println(humidity);
-  
-  delay(500);
-  if(humidity>initHumid+.5){
-    Serial.println("blue on");
-    switchLight(BLUEPIN,REDPIN);
-  }else{
-    Serial.println("red on");
-    switchLight(REDPIN,BLUEPIN);  
-  }
-}
 
-void switchLight(int onPin, int offPin){
- digitalWrite(onPin, HIGH);   // turn the LED on (HIGH is the voltage level)
- digitalWrite(offPin, LOW);   // turn the LED on (HIGH is the voltage level)
- 
+  //read the humidity
+  float humidity = dht.readHumidity();
+
+   // Read temperature as Fahrenheit (isFahrenheit = true)
+  float temp = dht.readTemperature(true);
+
+  //display data
+  Serial.print(humidity);
+  Serial.print("\t");
+  Serial.println(temp);
+  //wait x milliseconds so printout it slower
+  delay(1000);
+  
 }
